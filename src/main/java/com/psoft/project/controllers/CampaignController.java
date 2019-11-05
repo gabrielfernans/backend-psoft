04a9d4109e6a857b63778aa3cf781985b339ef4c
deboraleda <debora.souza@ccc.ufcg.edu.br>
2019-11-04 23:47:37 -0300

    metodo adicionaCampanha completo

--- src/main/java/com/psoft/project/controllers/CampaignController.java
+++ src/main/java/com/psoft/project/controllers/CampaignController.java
@@ -1,6 +1,24 @@
 package com.psoft.project.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.psoft.project.entities.Campaign;
+import com.psoft.project.services.CampaignService;
+
 public class CampaignController {
+	@Autowired
+	private CampaignService campaignService;
 	
+	
+	@PostMapping("/campaigns")
+	public ResponseEntity<Campaign> setCampaign(@RequestBody @Valid Campaign campaign) {
+		return new ResponseEntity<Campaign>(campaignService.setCampaign(campaign), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/psoft/project/services/CampaignService.java
+++ src/main/java/com/psoft/project/services/CampaignService.java
@@ -1,5 +1,17 @@
 package com.psoft.project.services;
 
+import com.psoft.project.entities.Campaign;
+import com.psoft.project.repositories.CampaignRepository;
+
 public class CampaignService {
+	
+	private CampaignRepository<Campaign, Integer> campaigns;
+
+	public Campaign setCampaign(Campaign campaign) {
+		this.campaigns.save(campaign);
+		return campaign;
+	}
+	
+	
 
 }
